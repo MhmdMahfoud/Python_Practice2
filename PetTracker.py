698c3056dacaef7e1b39ab0157913d07ad8cee81
Mhmd Mahfoud <engmohammedmahfoud2001@gmail.com>
2025-07-01 13:12:50 +0300

    Adding method Mark Adopted

--- PetTracker.py
+++ PetTracker.py
@@ -8,4 +8,6 @@ class Pet:
     def display(self):
         status = "adopted" if self.adopted else "not adopted"
         print(f"{self.name} is a {self.age}-year-old {self.species} and is currently {status}.")
-
+    
+    def mark_adopted(self):
+        self.adopted = True
