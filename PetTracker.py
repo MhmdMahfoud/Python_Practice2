ccf21032c8af526ef323484115bd9b4cc60836ab
Mhmd Mahfoud <engmohammedmahfoud2001@gmail.com>
2025-07-01 13:09:24 +0300

    Adding A Method To Print All Pet

--- PetTracker.py
+++ PetTracker.py
@@ -4,3 +4,8 @@ class Pet:
         self.species = species
         self.age = age
         self.adopted = adopted
+
+    def display(self):
+        status = "adopted" if self.adopted else "not adopted"
+        print(f"{self.name} is a {self.age}-year-old {self.species} and is currently {status}.")
+
